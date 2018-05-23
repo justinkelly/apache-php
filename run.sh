419e1936bf6d13951e9fd17c85a39dde06daf2a0
Justin Kelly <justin@kelly.org.au>
2018-05-23 15:29:11 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,5 +11,6 @@ fi
 source /etc/apache2/envvars
 /s3 --region "${AWS_REGION}" sync s3://docker-files.invoice.im/${DOMAIN}/logo/ /app/templates/invoices/logos/
 /s3 --region "${AWS_REGION}" sync s3://docker-files.invoice.im/${DOMAIN}/template/ /app/templates/invoices/
+php_value memory_limit 192M
 tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
